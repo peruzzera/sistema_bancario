1ce8ca4bc61182bf2070954b00ff39cbb70b060e
Pedro Peruzzo <pedrocperuzzo@gmail.com>
2025-07-16 01:59:06 -0300

    Update banco.py

--- banco.py
+++ banco.py
@@ -11,46 +11,45 @@ extrato = ""
 saques = 0
 limite_saque = 3
 
-while True: 
-
+while True:
     opcao = input(menu)
 
     if opcao == "1":
         valor = float(input("Informe o valor: "))
+
         if valor > 0:
             saldo += valor
             extrato += f"Deposito: R$ {valor:.2f}\n"
             print(f"Depósito de R$ {valor:.2f} realizado")
         else:
             print("Saldo insuficiente.")
-    
+
     elif opcao == "2":
         valor = float(input("Informe o valor do saque: "))
 
-        exc_saldo = valor > saldo
-        exc_limite = valor > limite
-        exc_saques = saques >= limite_saque
+        excedeu_saldo = valor > saldo
+        excedeu_limite = valor > limite
+        excedeu_saques = saques >= limite_saque
 
-        if exc_saldo:
-            print ("Saldo insuficiente")
-        
-        elif exc_limite:
-            print ("O saque deve ser de no maximo R$500")
-        
-        elif exc_saques:
-            print ("Maximo de saques atingido")
-        
+        if excedeu_saldo:
+            print("Saldo insuficiente")
+        elif excedeu_limite:
+            print("O saque deve ser de no maximo R$500")
+        elif excedeu_saques:
+            print("Maximo de saques atingido")
         elif valor > 0:
             saldo -= valor
             extrato += f"Saque: R$ {valor:.2f}\n"
             saques += 1
-        
         else:
             print("Valor invalido")
-    
+
     elif opcao == "3":
         print("=====EXTRATO=====")
-        print("Extrato em branco." if not extrato else extrato)
+        if not extrato:
+            print("Extrato em branco.")
+        else:
+            print(extrato)
         print(f"\nSaldo: R$ {saldo:.2f}")
         print("=================")
 
